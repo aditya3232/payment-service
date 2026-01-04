890bdf357bafc42cb2c64d75c114c3eb05ccc3e3
adit3232 <m.aditya3232@gmail.com>
2026-01-04 16:23:10 +0700

    add config

---
+++ config/config.go
@@ -0,0 +1,59 @@
+package config
+
+import (
+	"os"
+	"payment-service/common/util"
+
+	"github.com/sirupsen/logrus"
+)
+
+var Config AppConfig
+
+type AppConfig struct {
+	Port                  int             `json:"port"`
+	AppName               string          `json:"appName"`
+	AppEnv                string          `json:"appEnv"`
+	Database              Database        `json:"database"`
+	RateLimiterMaxRequest float64         `json:"rateLimiterMaxRequest"`
+	RateLimiterTimeSecond int             `json:"rateLimiterTimeSecond"`
+	Kafka                 Kafka           `json:"kafka"`
+	InternalService       InternalService `json:"internalService"`
+}
+
+type Database struct {
+	Host                  string `json:"host"`
+	Port                  int    `json:"port"`
+	Name                  string `json:"name"`
+	Username              string `json:"username"`
+	Password              string `json:"password"`
+	MaxOpenConnections    int    `json:"maxOpenConnections"`
+	MaxLifeTimeConnection int    `json:"maxLifeTimeConnection"`
+	MaxIdleConnections    int    `json:"maxIdleConnections"`
+	MaxIdleTime           int    `json:"maxIdleTime"`
+}
+
+type Kafka struct {
+	Brokers     []string `json:"brokers"`
+	TimeoutInMS int      `json:"timeoutInMS"`
+	MaxRetry    int      `json:"maxRetry"`
+	Topic       string   `json:"topic"`
+}
+
+type Customer struct {
+	Host string `json:"host"`
+}
+
+type InternalService struct {
+	Customer Customer `json:"customer"`
+}
+
+func Init() {
+	err := util.BindFromJSON(&Config, "config.json", ".")
+	if err != nil {
+		logrus.Infof("failed to bind config: %v", err)
+		err = util.BindFromConsul(&Config, os.Getenv("CONSUL_HTTP_URL"), os.Getenv("CONSUL_HTTP_PATH"))
+		if err != nil {
+			panic(err)
+		}
+	}
+}
--- go.mod
+++ go.mod
@@ -2,11 +2,20 @@ module payment-service
 
 go 1.25.5
 
-require github.com/gin-gonic/gin v1.11.0
+require (
+	github.com/gin-gonic/gin v1.11.0
+	gorm.io/gorm v1.25.10
+)
 
 require (
 	github.com/fsnotify/fsnotify v1.9.0 // indirect
 	github.com/go-viper/mapstructure/v2 v2.4.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/pgx/v5 v5.6.0 // indirect
+	github.com/jackc/puddle/v2 v2.2.2 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/patrickmn/go-cache v2.1.0+incompatible // indirect
 	github.com/sagikazarmark/locafero v0.11.0 // indirect
 	github.com/sourcegraph/conc v0.3.1-0.20240121214520-5f936abd7ae8 // indirect
@@ -27,7 +36,7 @@ require (
 	github.com/gin-contrib/sse v1.1.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
-	github.com/go-playground/validator/v10 v10.27.0 // indirect
+	github.com/go-playground/validator/v10 v10.27.0
 	github.com/goccy/go-json v0.10.2 // indirect
 	github.com/goccy/go-yaml v1.18.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
@@ -53,4 +62,5 @@ require (
 	golang.org/x/text v0.28.0 // indirect
 	golang.org/x/tools v0.35.0 // indirect
 	google.golang.org/protobuf v1.36.9 // indirect
+	gorm.io/driver/postgres v1.6.0
 )
