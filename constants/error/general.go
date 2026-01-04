0f6e73d2bb3de29838f58acef2b1784f7ef43bf4
adit3232 <m.aditya3232@gmail.com>
2026-01-04 16:12:51 +0700

    add constants

---
+++ constants/error/general.go
@@ -0,0 +1,17 @@
+package error
+
+import "errors"
+
+var (
+	ErrInternalServerError = errors.New("internal server error")
+	ErrSQLError            = errors.New("database server failed to execute query")
+	ErrTooManyRequests     = errors.New("too many requests")
+	ErrForbidden           = errors.New("forbidden")
+)
+
+var GeneralErrors = []error{
+	ErrInternalServerError,
+	ErrSQLError,
+	ErrTooManyRequests,
+	ErrForbidden,
+}
